429738177eb7db19516c6fc7633ce8f80456fa4b
Kapil Verma <kapilvermasgnand1@gmail.com>
2017-01-21 10:40:04 +0530

    changed torrent to torrentadmin as possible user_role removed from dashboard migratin added for survey table assign survey to torrent users by super/devadmin changed in surveylog now comment is saved in db improved audit log

--- app/SurveyLog.php
+++ app/SurveyLog.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class SurveyLog extends Model
 {
-    public $fillable = ['user_id', 'a_survey_id','is_status','ip_address'];
+    public $fillable = ['user_id', 'a_survey_id','is_status','ip_address',"comment"];
 }
